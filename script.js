a0025edaf07307746cb9628d444c46cc4ea44cce
Salman Ahmed <salmanahmedx@hotmail.com>
2022-04-28 00:27:58 +0600

    bug fix?

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const icon = document.querySelector(".icon")
 const input = document.querySelector(".input")
 
+let expand = false;
+
 icon.addEventListener('click', e => {
-    input.classList.toggle("input-expand")
+    if (!expand) {
+        input.style.width = "20vw";
+        expand = true;
+    } else {
+        input.style.width = "0vw"
+        expand = false;
+    }
 })
